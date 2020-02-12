56678154d9aa30afb6a4f3141683148ea7ca797f
Paul Han <paul.myung.han@gmail.com>
2020-02-11 22:03:45 -0800

    finished the search and clear function, started ajax function

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,36 @@
-// api.openweathermap.org/data/2.5/weather?q={city name}&appid={your api key}
+let currentLocation;
 
-let apiKey = "56e230dcae6b8601830ff7faa6ebd840";
+$("#searchbtn").on("click",function(){
+    event.preventDefault();
+    let loc = $("#search-input").val().trim();
+    //if location isnt empty
+    if (loc !== ""){
+        clearCity();
+        currentLoc = loc;
+        //clears search field value
+        $("search-input").val("");
+        getWeather(loc); 
+    }
+})
+
+function clearCity(){
+    //clears weather for current location
+    $(".weather").empty();
+
+}
 
 
+function getWeather(city){
+    let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=56e230dcae6b8601830ff7faa6ebd840";
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
+        let currentCard = $("<div>").attr("class", "card bg-light");
+        $(".weather").append(currentCard)
+      });
+}
 
+
+getWeather();
 
