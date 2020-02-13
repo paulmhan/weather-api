4597aa73d8bd8563e9193ab930d11f80e19b9961
Paul Han <paul.myung.han@gmail.com>
2020-02-13 13:52:36 -0800

    finished get weather function

--- assets/script.js
+++ assets/script.js
@@ -26,19 +26,32 @@ function getWeather(city){
         $(".weather").append(currentCard);
         let cityName = $("<div>").attr("class", "name").text(`Current weather for ${response.name}`);
         currentCard.append(cityName);
-        console.log(response);
         
         // gets info
         let textDiv = $("<div>").attr("class", "col-md-8");
         let cardBody = $("<div>").attr("class", "card-body");
         textDiv.append(cardBody);
         cardBody.append($("<h3>").attr("class", "card-title").text(response.name));
-        cardBody.append($("<p>").attr("class", "card-text").html("Temperature: " + response.main.temp));
-        console.log(response.main.temp);
+        let tempF = response.main.temp * 1.8 - 459.67;
+        tempF =  Math.round(tempF*10)/10
+        cardBody.append($("<p>").attr("class", "card-text").html("Temperature: " + tempF + "F"));
+        console.log(tempF);
         cardBody.append($("<p>").attr("class", "card-text").text("Humidity: " + response.main.humidity + "%"));
         cardBody.append($("<p>").attr("class", "card-text").text("Wind Speed: " + response.wind.speed + " MPH"));
         $(".weather").append(textDiv);
         
+
+        //uv index
+        let uvURL = "https://api.openweathermap.org/data/2.5/uvi?appid=7e4c7478cc7ee1e11440bf55a8358ec3&lat=" + response.coord.lat + "&lon=" + response.coord.lat;
+        $.ajax({
+            url: uvURL,
+            method: "GET"
+        }).then(function (uvresponse) {
+            let uvindex = uvresponse.value;
+            let uvdisp = $("<p>").attr("class", "card-text").text("UV Index: ");
+            uvdisp.append($("<span>").attr("class", "uvindex").text(uvindex));
+            cardBody.append(uvdisp);
+        });
       });
 }
 
